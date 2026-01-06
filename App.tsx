8ccfe4d29a6000b697dfc929118fd28b08db967c
Matheus-D-R <maatheusdiasramos@gmail.com>
2026-01-06 03:34:39 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import SalesPage from './components/SalesPage';
 
+
+
+import { useEffect } from 'react'
+
+function App() {
+
+  useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView')
+    }
+  }, [])
+
+  return (
+    <>
+      {/* seu conteúdo */}
+    </>
+  )
+}
+
+export default App
+
+
+
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-brand-dark text-white">
